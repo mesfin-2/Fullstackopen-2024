88c859d114cba6370aa7668f37a7c3e1eadb79ff
mesfin-2 <mesfin_meng@yahoo.com>
2024-02-21 09:12:00 +0200

    note form added

--- part2/notesApp/src/App.jsx
+++ part2/notesApp/src/App.jsx
@@ -1,14 +1,37 @@
+import React, { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+
+    //add new note
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5, //50% chance being important
+      id: notes.length + 1,
+    };
+    //setNotes(notes.concat(noteObject))
+    setNotes({ ...notes, noteObject });
+    setNotes("");
+    console.log(newNote);
+  };
+  const onNoteChange = (e) => {
+    setNewNote(e.target.value);
+  };
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
+        {notes && notes.map((note) => <Note key={note.id} note={note} />)}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={onNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
