3bba61f223ccb04db074a4ab294e469e41acbad8
mesfin-2 <mesfin_meng@yahoo.com>
2024-02-22 10:14:21 +0200

    getAll,create and update requests applied also error handling

--- part2/notesApp/src/App.jsx
+++ part2/notesApp/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import noteService from "./services/notes.js";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,13 +10,13 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
+    noteService.getAll().then((initialNotes) => {
       console.log("promise fulfilled");
 
-      setNotes(response?.data);
+      setNotes(initialNotes);
     });
   }, []);
-  console.log("render", notes.length, "notes");
+  //console.log("render", notes.length, "notes");
 
   const addNote = (e) => {
     e.preventDefault();
@@ -23,12 +25,15 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5, //50% chance being important
-      id: notes.length + 1,
+      id: uuidv4(),
     };
-    //setNotes(notes.concat(noteObject))
-    setNotes({ ...notes, noteObject });
-    setNotes("");
-    console.log(newNote);
+    noteService.create(noteObject).then((returnedNote) => {
+      //setNotes(notes.concat(returnedNote));
+      setNotes([...notes, returnedNote]);
+
+      setNewNote("");
+      console.log(newNote);
+    });
   };
   const onNoteChange = (e) => {
     setNewNote(e.target.value);
@@ -36,6 +41,32 @@ const App = () => {
 
   const onToggleShow = () => {
     setShowAll(!showAll);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    //create a new object that is an exact copy of the old note,
+    //apart from the important property that has the value flipped (from true to false or from false to true).
+
+    const changeNote = { ...note, important: !note.important };
+    //he new note is then sent with a PUT request to the backend
+    //After the request is successful, it updates the notes state by mapping over
+    //the existing notes array. If the id of the note matches the id provided,
+    //it replaces that note with the updated note received from the server (response.data).
+
+    noteService
+      .update(id, changeNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        //deleted note gets filtered out from the state.
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+
+    console.log(`importance of   ${id}  needs to be toggled`);
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -46,9 +77,14 @@ const App = () => {
         show {showAll ? "important" : "all"}
       </button>
       <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
+        {Array.isArray(notesToShow) && // Check if notesToShow is an array
+          notesToShow.map((note) => (
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={onNoteChange} />
