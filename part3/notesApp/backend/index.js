454e384d8a86bf8c459d14de140e7444385ddea9
mesfin-2 <mesfin_meng@yahoo.com>
2024-02-27 00:09:26 +0200

    implement express backend on noteapp

--- part3/notesApp/backend/index.js
+++ part3/notesApp/backend/index.js
@@ -1,4 +1,8 @@
 const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -18,10 +22,57 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+app.get("/", (req, res) => {
+  res.send("<h2>Hello World</h2>");
 });
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.status(200).json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "application/json" });
+//   response.end(JSON.stringify(notes));
+// });
 
 const PORT = 3001;
 app.listen(PORT);
