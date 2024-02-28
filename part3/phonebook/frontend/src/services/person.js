fafa2944cda980dc11d08c5a28218096e4d6dc13
mesfin-2 <mesfin_meng@yahoo.com>
2024-02-28 19:07:26 +0200

    uibuild

--- part3/phonebook/frontend/src/services/person.js
+++ part3/phonebook/frontend/src/services/person.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/persons";
+const baseURL = "api/persons";
+//const baseURL = "http://localhost:3001/api/persons";
+//const baseURL = "https://fullstackopen-2024-phonebook.onrender.com/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseURL);
