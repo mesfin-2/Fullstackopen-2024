ec298af1f140e36b02c8316727805e39ccbfeb87
mesfin-2 <mesfin_meng@yahoo.com>
2024-03-16 00:43:42 +0200

    4.14 List Expansions, step2 Refactoring Test implementation

--- part4/blog/backend/tests/blog_api.test.js
+++ part4/blog/backend/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("assert");
 const supertest = require("supertest");
 const app = require("../app");
@@ -7,147 +7,132 @@ const Blog = require("../models/blog");
 const helpers = require("./test_helpers");
 const api = supertest(app);
 
-//before
+// Define a describe block for blog-related tests
+describe("Blog API", () => {
+  // Define a beforeEach hook to initialize the database
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    const blogObjects = helpers.initialBlogs.map((blog) => new Blog(blog));
+    const promiseArray = blogObjects.map((blog) => blog.save());
+    await Promise.all(promiseArray);
+  });
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  /*
-    The noteObjects variable is assigned to an array of Mongoose objects that are 
-    created with the Note constructor for each of the notes in the helper.initialNotes array.
-    */
+  // Define a describe block for GET requests
+  describe("GET requests", () => {
+    test("There is only three blog posts", async () => {
+      const response = await api.get("/api/blogs");
+      assert.strictEqual(response.body.length, helpers.initialBlogs.length);
+    });
 
-  const blogObjects = helpers.initialBlogs.map((blog) => new Blog(blog));
-  /*
-     promiseArray=> creates a new array that consists of promises, that are created by calling the save method 
-     of each item in the noteObjects array. In other words, it is an array of promises for saving 
-     each of the items to the database.
-    
-    */
-  const promiseArray = blogObjects.map((blog) => blog.save());
-  /*
-    The Promise.all method can be used for transforming an array of promises into a single promise, that will be fulfilled
-     once every promise in the array passed to it as a parameter is resolved
-    - Promise.all(promiseArray) waits until every promise for saving a note is finished, meaning that the database 
-    has been initialized.
-    */
-  await Promise.all(promiseArray);
-});
+    test("blogs are returned as json format", async () => {
+      await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+    });
 
-test("There is only three blog posts", async () => {
-  const response = await api.get("/api/blogs");
+    test("Each blog post have id property instead of _id", async () => {
+      const response = await api.get("/api/blogs");
+      const blogs = response.body;
+      console.log("id-Check", Object.hasOwn(blogs[0], "id"));
 
-  assert.strictEqual(response.body.length, helpers.initialBlogs.length);
-});
+      blogs.forEach((blog) => {
+        assert.strictEqual(Object.hasOwn(blogs[0], "id"), true);
+        assert.strictEqual(Object.hasOwn(blogs[0], "_id"), false);
+      });
+    });
+  });
 
-test("blogs are returned as json format", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  // Define a describe block for POST requests
+  describe("POST requests", () => {
+    test("add a valid blog post ", async () => {
+      const newBlog = {
+        title: "MERN-Stack app development",
+        author: "Mesfin M",
+        url: "https://blog.nextideatech.com/how-to-get-started-with-the-mern-stack-a-comprehensive-guide/",
+        likes: 2,
+      };
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-test("Each blog post have id property instead of _id", async () => {
-  const response = await api.get("/api/blogs");
-  const blogs = response.body;
-  console.log("id-Check", Object.hasOwn(blogs[0], "id"));
+      const blogsAtEnd = await helpers.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length + 1);
+      const contents = blogsAtEnd.map((n) => n.title);
+      assert(contents.includes(newBlog.title));
+    });
 
-  blogs.forEach((blog) => {
-    assert.strictEqual(Object.hasOwn(blogs[0], "id"), true);
-    assert.strictEqual(Object.hasOwn(blogs[0], "_id"), false);
+    test("likes property is missing from the request", async () => {
+      const newBlog = {
+        title: "FullStack app development2",
+        author: "Mesfin M",
+        url: "https://blog.nextideatech.com/how-to-get-started-with-the-mern-stack-a-comprehensive-guide/",
+      };
+      await api
+        .post("/api/blogs")
+        .send({ ...newBlog, likes: 0 })
+        .expect(201);
+      const blogsAtEnd = await Blog.find({});
+      assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length + 1);
+    });
+
+    test("title or url properties are missing from the request ", async () => {
+      const newBlog = {
+        author: "Mesfin M",
+        likes: 2,
+      };
+      await api.post("/api/blogs").send(newBlog).expect(400);
+      const blogsAtEnd = await helpers.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length);
+    });
+  });
+
+  // Define a describe block for DELETE requests
+  describe("DELETE requests", () => {
+    test("delete single blog post", async () => {
+      const blogsAtStart = await helpers.blogsInDb();
+      console.log("blogAtStart", blogsAtStart);
+      const blogToDelete = blogsAtStart[0];
+      console.log("blogToDelete", blogToDelete);
+
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+      const blogsAtEnd = await helpers.blogsInDb();
+
+      assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length - 1);
+
+      const contents = blogsAtEnd.map((r) => r.title);
+      assert(!contents.includes(blogToDelete.title));
+    });
+  });
+
+  // Define a describe block for PUT requests
+  describe("PUT requests", () => {
+    test("update a single blog likes", async () => {
+      const blogsAtStart = await helpers.blogsInDb();
+      const blogToUpdate = blogsAtStart[0];
+      const updatedLikes = 4;
+
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send({ likes: updatedLikes })
+        .expect(204);
+
+      const blogsAtEnd = await helpers.blogsInDb();
+
+      const updatedBlog = blogsAtEnd.find(
+        (blog) => blog.id === blogToUpdate.id
+      );
+      console.log("Updated blog:", updatedBlog);
+
+      assert.strictEqual(updatedBlog.likes, updatedLikes);
+    });
   });
 });
 
-// Test adding a valid blog post
-test("add a valid blog post ", async () => {
-  const newBlog = {
-    title: "MERN-Stack app development",
-    author: "Mesfin M",
-    url: "https://blog.nextideatech.com/how-to-get-started-with-the-mern-stack-a-comprehensive-guide/",
-    likes: 2,
-  };
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  //check the state stored in the database after the saving operation, by fetching all the blogs of the application.
-  const blogsAtEnd = await helpers.blogsInDb();
-
-  assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length + 1);
-
-  // Convert blog objects to JSON
-  //const blogObjects = blogsAtEnd.map((blog) => blog.toJSON());
-
-  // Check if the new blog post's title is included in the saved blogs
-  const contents = blogsAtEnd.map((n) => n.title);
-  assert(contents.includes(newBlog.title));
-});
-
-//Test if the likes is missing and add a default value of 0
-test("likes property is missing from the request", async () => {
-  const newBlog = {
-    title: "FullStack app development2",
-    author: "Mesfin M",
-    url: "https://blog.nextideatech.com/how-to-get-started-with-the-mern-stack-a-comprehensive-guide/",
-  };
-  await api
-    .post("/api/blogs")
-    .send({ ...newBlog, likes: 0 })
-    .expect(201);
-  //check the state stored in the database after the saving operation, by fetching all the notes of the application.
-  const blogsAtEnd = await Blog.find({});
-  assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length + 1);
-});
-
-test("title or url properties are missing from the request ", async () => {
-  const newBlog = {
-    author: "Mesfin M",
-    likes: 2,
-  };
-  await api.post("/api/blogs").send(newBlog).expect(400);
-  //check the state stored in the database after the saving operation, by fetching all the notes of the application.
-  const blogsAtEnd = await helpers.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length);
-});
-
-test("delete single blog post", async () => {
-  const blogsAtStart = await helpers.blogsInDb();
-  console.log("blogAtStart", blogsAtStart);
-  const blogToDelete = blogsAtStart[0];
-  console.log("blogToDelete", blogToDelete);
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await helpers.blogsInDb();
-
-  assert.strictEqual(blogsAtEnd.length, helpers.initialBlogs.length - 1);
-
-  const contents = blogsAtEnd.map((r) => r.title);
-  assert(!contents.includes(blogToDelete.title));
-});
-
-test("update a single blog likes", async () => {
-  const blogsAtStart = await helpers.blogsInDb();
-  const blogToUpdate = blogsAtStart[0];
-  const updatedLikes = 4; // Define the updated likes value
-
-  await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send({ likes: updatedLikes }) // Send the updated likes value in the request body
-    .expect(204);
-
-  const blogsAtEnd = await helpers.blogsInDb();
-
-  // Find the updated blog in the array
-  const updatedBlog = blogsAtEnd.find((blog) => blog.id === blogToUpdate.id);
-  console.log("Updated blog:", updatedBlog);
-
-  // Assert that the likes of the updated blog match the updatedLikes value
-  assert.strictEqual(updatedBlog.likes, updatedLikes);
-});
-
-//after Each test
+// Define an after hook to close the database connection
 after(async () => {
   await mongoose.connection.close();
 });
